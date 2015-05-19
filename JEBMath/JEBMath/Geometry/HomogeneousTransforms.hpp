3028416e968cd447badc79299f7d640fe6ee157a
Jan Erik Breimo <jebreimo@gmail.com>
2015-05-19 21:02:16 +0200

    Made the homogeneous transforms more flexible

--- JEBMath/JEBMath/Geometry/HomogeneousTransforms.hpp
+++ JEBMath/JEBMath/Geometry/HomogeneousTransforms.hpp
@@ -5,20 +5,145 @@
 
 namespace JEBMath { namespace HomogeneousTransforms {
 
-Matrix<double, 4> scale4D(double s);
-Matrix<double, 4> scale4D(const Vector<double, 3>& scales);
-Matrix<double, 4> scale4DTrans(double s);
-Matrix<double, 4> scale4DTrans(const Vector<double, 3>& scales);
-Matrix<double, 4> rotateZ4D(double angle);
-Matrix<double, 4> rotateZ4DTrans(double angle);
-Matrix<double, 4> rotateY4D(double angle);
-Matrix<double, 4> rotateY4DTrans(double angle);
-Matrix<double, 4> rotateX4D(double angle);
-Matrix<double, 4> rotateX4DTrans(double angle);
-Matrix<double, 4> rotate4D(double angle, const Vector<double, 3>& axis);
-Matrix<double, 4> rotate4DTrans(double angle, const Vector<double, 3>& axis);
-Matrix<double, 4> translate4D(double x, double y, double z);
-Matrix<double, 4> translate4DTrans(double x, double y, double z);
+template <typename T, unsigned N>
+Matrix<T, N> scale(T s)
+{
+    Matrix<T, N> m;
+    for (unsigned i = 0; i < N - 1; ++i)
+        m[i][i] = s;
+    m[N - 1][N - 1] = 1;
+    return m;
+}
+
+template <typename T, unsigned N>
+Matrix<T, N> scale(const Vector<T, N - 1>& s)
+{
+    Matrix<T, N> m;
+    for (unsigned i = 0; i < N - 1; ++i)
+        m[i][i] = s[i];
+    m[N - 1][N - 1] = 1;
+    return m;
+}
+
+template <typename T, unsigned N>
+Matrix<T, N> rotateZ(T angle)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    Matrix<T, N> m;
+    m[0][0] = m[1][1] = c;
+    m[0][1] = -s;
+    m[1][0] = s;
+    for (unsigned i = 2; i < N; ++i)
+        m[i][i] = 1;
+    return m;
+}
+
+template <typename T, unsigned N>
+Matrix<T, N> rotateZT(T angle)
+{
+    auto m = rotateZ(angle);
+    std::swap(m[0][1], m[1][0]);
+    return m;
+}
+
+template <typename T>
+Matrix<T, 4> rotateY4(T angle)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    return Matrix<T, 4>{ c, 0, s, 0,
+                         0, 1, 0, 0,
+                        -s, 0, c, 0,
+                         0, 0, 0, 1};
+}
+
+template <typename T>
+Matrix<T, 4> rotateY4T(T angle)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    return Matrix<T, 4>{c, 0, -s, 0,
+                        0, 1,  0, 0,
+                        s, 0,  c, 0,
+                        0, 0,  0, 1};
+}
+
+template <typename T>
+Matrix<T, 4> rotateX4(T angle)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    return Matrix<T, 4>{1,  0, 0, 0,
+                        0,  c, s, 0,
+                        0, -s, c, 0,
+                        0,  0, 0, 1};
+}
+
+template <typename T>
+Matrix<T, 4> rotateX4T(T angle)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    return Matrix<T, 4>{1, 0,  0, 0,
+                        0, c, -s, 0,
+                        0, s,  c, 0,
+                        0, 0,  0, 1};
+}
+
+template <typename T>
+Matrix<T, 4> rotate4(T angle, const Vector<T, 3>& axis)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    auto cx = axis * ((1 - c) * axis[0]);
+    auto cy = axis * ((1 - c) * axis[1]);
+    auto cz = axis * ((1 - c) * axis[2]);
+    auto sa = s * axis;
+    return Matrix<T, 4>{cx[0] + c,     cx[1] - sa[2], cx[2] + sa[1], 0,
+                        cy[0] + sa[2], cy[1] + c,     cy[2] - sa[0], 0,
+                        cz[0] - sa[1], cz[1] + sa[0], cz[2] + c,     0,
+                                    0,             0,         0,     1};
+}
+
+template <typename T>
+Matrix<T, 4> rotate4T(T angle, const Vector<T, 3>& axis)
+{
+    auto c = std::cos(angle);
+    auto s = std::sin(angle);
+    auto cx = axis * ((1 - c) * axis[0]);
+    auto cy = axis * ((1 - c) * axis[1]);
+    auto cz = axis * ((1 - c) * axis[2]);
+    auto sa = s * axis;
+    return Matrix<T, 4>{cx[0] + c,     cy[0] + sa[2], cz[0] - sa[1], 0,
+                        cx[1] - sa[2], cy[1] + c,     cz[1] + sa[0], 0,
+                        cx[2] + sa[1], cy[2] - sa[0], cz[2] + c,     0,
+                                    0,             0,         0,     1};
+}
+
+template <typename T, unsigned N>
+Matrix<T, N> translate(const Vector<T, N - 1>& offsets)
+{
+    Matrix<T, N> m;
+    for (unsigned i = 0; i < N - 1; ++i)
+    {
+        m[i][i] = 1;
+        m[i][N - 1] = offsets[i];
+    }
+    m[N - 1][N - 1] = 1;
+    return m;
+}
+
+template <typename T, unsigned N>
+Matrix<T, N> translateT(const Vector<T, N - 1>& offsets)
+{
+    Matrix<T, N> m;
+    for (unsigned i = 0; i < N; ++i)
+        m[i][i] = 1;
+    for (unsigned i = 0; i < N - 1; ++i)
+        m[N - 1][i] = offsets[i];
+    return m;
+}
 
 }}
 
